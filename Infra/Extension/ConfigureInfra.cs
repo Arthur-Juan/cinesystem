1cb4defc0679f54c1477c90e8a5b9dc30b188bb3
Arthur Lima <arthur.lima@inforce.com.br>
2023-08-04 23:21:23 -0300

    initial with dependencies

---
+++ Infra/Extension/ConfigureInfra.cs
@@ -0,0 +1,26 @@
+﻿using Infra.Data.Efcore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+
+
+namespace Infra.Extension;
+
+public static class ConfigureInfra
+{
+    public static void ConfigInfra(this IServiceCollection services, string connection)  
+    {
+        services.AddDbContext<AppDbContext>(opt =>
+        {
+            opt.UseSqlServer(connection);
+        });
+
+        services.AddAutoMapper();
+    }
+}
---
+++ WebApi/Program.cs
@@ -0,0 +1,34 @@
+using Infra.Extension;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.ConfigInfra("xpto");
+
+var log = new LoggerConfiguration()
+    .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
