c898da35f749f32b02fde1fb4400a5c8ab7d2f4c
Arthur Lima <arthur.lima@inforce.com.br>
2023-08-20 18:01:48 -0300

    refact: CreatedAt como string

--- Domain/Entities/Entity.cs
+++ Domain/Entities/Entity.cs
@@ -8,8 +8,9 @@ namespace Domain.Entities;
 
 public class Entity
 {
+ 
     public Guid Id { get; set; } = Guid.NewGuid();
-    public DateTime CreatedAt { get; set; }
-    public DateTime UpdatedAt { get; set; }
+    public string CreatedAt { get; set; } = DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss.fff");
+    //public string UpdatedAt { get; set; } = DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss.fff");
 
 }
