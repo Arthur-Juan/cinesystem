393879d87a43325d0a21ab6243bac2380bec14e1
Arthur Lima <arthur.lima@inforce.com.br>
2023-08-20 18:00:20 -0300

    refact: add Id em UserLOggedDTO

--- Core/DTOs/Response/UserLoggedDTO.cs
+++ Core/DTOs/Response/UserLoggedDTO.cs
@@ -3,6 +3,7 @@
 namespace Application.DTOs.Response;
 
 public record UserLoggedDTO(
+    Guid? Id,
     string? FirstName,
     string? LastName,
     string? Email,
@@ -12,6 +13,7 @@ public record UserLoggedDTO(
     public static UserLoggedDTO mapFromEntity(User? entity, string? token)
     {
         return new UserLoggedDTO(
+            entity?.Id,
             entity?.FirstName,
             entity?.LastName,
             entity?.Email,
